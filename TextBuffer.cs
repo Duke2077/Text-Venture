0f742de66ba502b4c94b455a3ba61f6a8efea167
Duke2077 <133334798+Duke2077@users.noreply.github.com>
2023-05-12 10:04:46 +0200

    Create TextBuffer.cs

---
+++ TextBuffer.cs
@@ -0,0 +1,38 @@
+  //using Console; //pour Write
+
+  namespace TheHyperionProject
+  {
+    /// <summary>
+    ///   the text buffer class
+    ///
+    ///   Contains text.
+    /// </summary>
+    static class TextBuffer
+    {
+      private static string outputBuffer; /**< the text content */
+    
+    
+      /// <summary>
+      ///   Adds text to the buffer.
+      /// </summary>
+      public static void AddTextToBuffer(string text)
+      {
+        outputBuffer += text + "\n";
+      }
+
+      /// <summary>
+      ///   Displays the content of the buffer.
+      ///
+      ///   Displays to the screen.
+      /// </summary>
+      public static void Display()
+      {
+        Console.Clear();
+        Console.Write(TextUtilities.WordWrap(outputBuffer, Console.WindowWidth));
+        //Console.WriteLine("Please enter a command (or type \"exit\" to finish the game)");
+        //Console.Write("huy@DRAGOON:~/The Hyperion Project$ ");
+
+        outputBuffer = "";
+      }    
+    }
+  }
