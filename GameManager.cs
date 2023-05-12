71352f8a0edebf23ca0b159e0d3f675798fb2d0e
Duke2077 <133334798+Duke2077@users.noreply.github.com>
2023-05-12 10:01:47 +0200

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,108 @@
+  //using System; //without this using line, it still works
+                  //within and outside of VSCode 
+
+  namespace TheHyperionProject
+  {
+    /// <summary>
+    ///   The GameManager class.
+    ///
+    ///   Manages the game.
+    /// </summary>
+    static class GameManager
+    {
+      #region Public Member Functions
+
+      /// <summary>
+      ///   Displays a title screen.
+      ///
+      ///   Displays introductory information about the game.
+      /// </summary>
+      public static void ShowTitleScreen()
+      {
+        Console.Clear();
+        Console.WriteLine(TextUtilities.WordWrap("*** The Hyperion Project *** " +
+                                                 "A Text-based Adventure Game.\n",
+                                                 Console.WindowWidth));
+
+        Console.WriteLine(TextUtilities.WordWrap("Police Station, 2F. The second floor of the Police Station contains more long hallways and office rooms, some of which are locked - and the Chief's Room cannot be accessed in Leon's Story. Key Items on this floor include the Lion and Unicorn Emblem, second floor map, Lightning Hawk Magnum or Submachine Gun, two Electronic Locks, Detonator Battery, Weapons Locker Keycard, Heart Key (Claire Only) and Red Jewel.",
+                                                 Console.WindowWidth));
+
+        Console.WriteLine ("Info: You may type 'help' at any " +
+                           "time to see a list of available commands.");
+        Console.Write ("\nPress a key to begin");
+
+        Console.CursorVisible = false;
+        Console.ReadKey();
+        Console.CursorVisible = true;
+        Console.Clear();
+      }
+
+      /// <summary>
+      ///   Displays the level.
+      ///
+      ///   Objects have been placed inside of rooms. Rooms
+      ///   have been placed inside the level. And now it's time
+      ///   to start the game. This kicks off the display of the
+      ///   level.
+      /// </summary>
+      public static void StartGame()
+      {
+        Player.GetCurrentRoom().Describe();
+        TextBuffer.Display();
+      }
+
+      /// <summary>
+      ///   Ends the game.
+      ///
+      ///   Marks the program as ready to quit and then show
+      ///   the final ending message. Will then prevent any
+      ///   further input.
+      /// </summary>
+      public static void EndGame(string endingText)
+      {
+        ClassePrincipale.GameIsOn = false;
+
+        Console.Clear();
+        Console.WriteLine(TextUtilities.WordWrap(endingText, 
+                                                 Console.WindowWidth));
+        Console.WriteLine("\nYou may now close this window.");
+        Console.CursorVisible = false;
+
+        Console.ReadKey (true);
+      }
+
+      /// <summary>
+      ///   Applies our game's rules.
+      ///
+      ///   Sees if the player, objects and rooms are arranged
+      ///   in a way that triggers something special like an
+      ///   ending or loss of life.
+      /// </summary>
+      public static void ApplyRules()
+      {
+        if ((Level.Rooms[0, 0].ProvideItem("Red Ball") != null) &&
+          (Level.Rooms[1, 0].ProvideItem("Blue Ball") != null) &&
+            (Level.Rooms[1, 1].ProvideItem("Yellow Ball") != null) &&
+              (Level.Rooms[0, 1].ProvideItem("Green Ball") != null))
+        {
+          EndGame ("Well done! You win!");
+        }
+
+        if (Player.GetInventoryItem("Key") != null)
+        {
+          Level.Rooms[0, 0].UnlockExit(Direction.South); //unlock door to the south in red room
+          Level.Rooms[0, 0].Description = "You have entered the red room.";
+
+          Level.Rooms[0, 1].UnlockExit(Direction.North); //unlock door to the north in green room
+          Level.Rooms[0, 1].Description = "You have entered the green room.";
+        }
+
+        if (Player.PlayedMoves > 10)
+        {
+          EndGame("You have used your 10 possible moves. Game is now over.");
+        }
+      }
+    
+      #endregion
+    }
+  }
