c112db96d001c02f4d860865d0afd64eba622643
Duke2077 <133334798+Duke2077@users.noreply.github.com>
2023-05-13 10:37:25 +0200

    Update Direction.cs

--- Direction.cs
+++ Direction.cs
@@ -1,46 +1,36 @@
-using System.Collections.Generic; // List<T>
 
-namespace HeartbeatHunter
-{
-  /// <summary>
-  ///   The "Direction" C# struct
-  ///
-  ///   Represents the idea of north, south, east and west.
-  /// </summary>
-  struct Direction
+  namespace HeartbeatHunter
   {
-    public const string North = "north";
-    public const string South = "south";
-    public const string East = "east";
-    public const string West = "west";
-
     /// <summary>
-    ///   Tells us if a direction name exists.
+    ///   The "Direction" C# struct
+    ///
+    ///   Represents the idea of north, south, east and west.
     /// </summary>
-    public static bool IsValidDirection(string direction)
+    struct Direction
     {
-      switch (direction)
+      public const string North = "north";
+      public const string South = "south";
+      public const string East = "east";
+      public const string West = "west";
+
+      /// <summary>
+      ///   Tells us if a direction name exists.
+      /// </summary>
+      public static bool IsValidDirection(string direction)
       {
-        case Direction.North:
-          return true;
-        case Direction.South:
-          return true;
-        case Direction.East:
-          return true;
-        case Direction.West:
-          return true;
+        switch (direction)
+        {
+          case Direction.North:
+            return true;
+          case Direction.South:
+            return true;
+          case Direction.East:
+            return true;
+          case Direction.West:
+            return true;
+        }
+        return false;
       }
-      return false;
-    }
-
-    public static bool operator ==(Direction d1, Direction d2)
-    {
-      return d1.Value == d2.Value;
-    }
-
-    public static bool operator !=(Direction d1, Direction d2)
-    {
-      return d1.Value != d2.Value;
     }
   }
-}
+
